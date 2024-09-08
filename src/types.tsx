9107a1db6a20120e69a4d9bff4dafff551a6c1c3
VictoryPashkova <victory.pashkova@gmail.com>
2024-09-08 21:03:00 +0600

    chore: add types file

---
+++ src/types.tsx
@@ -0,0 +1,20 @@
+export type Product = {
+    id: number;
+    title: string;
+    images: string[];
+    price: number;
+    description: string;
+    brand: string;
+    dimensions: {
+      height: number;
+      width: number;
+      depth: number;
+    },
+    rating: number;
+    category: string;
+    isFavorite: boolean;
+  };
+
+  export type ProductsState = {
+    products: Product[];
+  };
