bc9d1141fb2a7abad796f4bfe1a3194567bce80d
VictoryPashkova <victory.pashkova@gmail.com>
2024-09-09 15:22:42 +0600

    refactor(CreateProductPage): replace button element with UIButton component

--- src/pages/CreateProductPage.tsx
+++ src/pages/CreateProductPage.tsx
@@ -1,23 +1,17 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Box, Button, Typography, Container } from '@mui/material';
+import { Box, Typography, Container } from '@mui/material';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import routes from '../routes';
 import CreateProductForm from '../components/сreateProductForm';
+import UIButton from '../ui/buttons/button';
 
 const CreateProductPage: React.FC = () => {
   const navigate = useNavigate();
 
   return (
     <Container maxWidth="xl">
-      <Button
-        startIcon={<ArrowBackIcon />}
-        variant="outlined"
-        onClick={() => navigate(routes.products())}
-        sx={{ mt: 4 }}
-      >
-        Back to products
-      </Button>
+      <UIButton btnText="Back to products" onClick={() => navigate(routes.products())} startIcon={<ArrowBackIcon />} variant='outlined' sx={{ mt: 4 }}/>
       <Box sx={{ mt: 4, p: 3, border: '1px solid #ddd', borderRadius: '8px' }}>
         <Typography variant="h4" gutterBottom>
           Create New Product
